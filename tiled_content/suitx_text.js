4723a4cbd0e6aad3dbd3bb429de650960d78a20b
benjaminnarin <benjamin.narin@gmail.com>
2017-12-26 01:57:52 -0800

    Moved tiled content from index to external js file

---
+++ tiled_content/suitx_text.js
@@ -0,0 +1,19 @@
+document.write('\
+<div class="modal-body">\
+		<!-- Project Details Go Here -->\
+		<div class="col-lg-12 text-center">\
+				<h2>SuitX</h2>\
+		</div>\
+		<img class="img-responsive img-centered" src="img/portfolio/thumbnails/phoenix_preview_1.jpg" alt="">\
+		<p></p>\
+		<p>\
+			&emsp;The Phoenix exoskeleton is the world\'s lightest and most advanced exoskeleton designed to help people with mobility disorders to be upright and mobile.\
+			In the clinic, at home, and in the workplace Phoenix has successfully enabled many individuals to stand up, walk about, and speak to peers eye-to-eye.\
+			Phoenix has only two actuators at its hip; the knee joints are designed to allow support during stance and ground clearance during swing.\
+		</p>\
+		\
+		<div class="text-center">\
+				<button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>\
+		</div>\
+</div>\
+');
