0862629deff6463b712a3c9c6be75d7880f9ef24
Benjamin Narin <benjamin.narin@gmail.com>
2018-01-02 12:06:10 -0800

    Updated Suitx text

--- research_work_content/suitx_text.js
+++ research_work_content/suitx_text.js
@@ -7,34 +7,36 @@ document.write('\
 		<img class="img-responsive img-centered" src="img/portfolio/thumbnails/phoenix_preview_1.jpg" alt="">\
 		<p></p>\
 		<p>\
-		&emsp;SuitX is a startup company working on both medical and industrial Exoskeletons. \
-		The company was spun off of Homayoon Kazerooni\'s lab at UC Berkeley.\
+		&emsp;SuitX is a startup company working on medical and industrial Exoskeletons. \
+		The company was created out of Homayoon Kazerooni\'s lab at UC Berkeley.\
 		<p>\
 		<p>\
-		&emsp;While competing in the Robotis For Good Competition in Dubai, with the\
+		&emsp;While competing in the Robotics For Good Competition in Dubai, with the\
 		wheelchair project, I met several members of suitX another team competing\
-		Afer getting to know them over the course of the competition I ended up going\
-		down to Berkeley to intern with them for the summer. During my time there I\
-		had the chance to work with their medical, industrial\
+		After getting to know the suitX team over the course of the competition they\
+		offered me a summer internship, thus I spent the summer in Berkeley\. Over the\
+		four months I was at suitX I had the chance to work with their medical, industrial\
 		and marketing teams.\
 		</p>\
 		<p>\
-		&emsp;While working with the Medical Exo Team over the four months I was at suitX I\
+		&emsp;As a part of their Medical Exo Team I\
 		updated, modified or designed all thirteen boards on the suit, resulting in\
 		a new run of PCBs and a new Exo Suit Revision. I also unified the\
 		electrical parts library, established design standards and practices and added\
 		revision control.\
 		</p>\
 		<p>\
-		&emsp;During my time working with the Industrial Engineering Team I helped review \
+		&emsp;I assisted the Industrial Engineering Team review \
 		and update electrical designs going into production. I also unified the electrical\
 		part library, PCB standards and workflow with those established for the Medical \
 		Exo Team.\
 		</p>\
 		<p>\
-		&emsp;Although I was hired on as an Electical Engineering Intern, I ended up doing\
-		working with the marketing team. During my time at suitX I attended two events\
-		the first was a contruction exo and the second was at Google Ventures.\
+		&emsp;Although My work with suitX went beyond that of an Electrical Engineering\
+		Intern, extending to collaborating with the Marketing Team. In addition to weekly\
+		interactions with investors and outside partners visiting suitX, I  attended two\
+		outreach events one at the Construction Expo and another at Google Ventures\
+
 		</p>\
 		\
 		<div class="text-center">\
